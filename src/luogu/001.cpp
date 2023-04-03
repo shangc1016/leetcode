c05857f03c717eed6e68cf1ed527d402130f877c
shangc1016 <shangc1016@163.com>
2023-04-04 00:00:08 +0800

    automatic push @2023年 04月 04日 星期二 00:00:08 CST

--- src/luogu/001.cpp
+++ src/luogu/001.cpp
@@ -1,30 +1,37 @@
 #include <algorithm>
+#include <chrono>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <iterator>
+#include <list>
+#include <locale>
 #include <map>
 #include <ostream>
-#include <vector>
+#include <queue>
 #include <unordered_map>
-#include <list>
+#include <vector>
+
 using namespace std;
 
+class Compare {
+ public:
+  bool operator()(int A, int B) { return A < B; }
+};
 int main() {
   //
   //
-  std::list<int> list_;
+  //
 
-  map<int, int> umap;
-
-  umap.insert({1, 1});
-  umap.insert({2, 1});
-  umap.insert({5, 1});
-
-  for (auto it : umap) {
-    cout << it.first << ", "<< it.second << std::endl;
+  for (int i = 0; i < 100000; i++) {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    uint64_t val = t1.time_since_epoch().count();
+    printf("%ld\n", val);
   }
 
-  //
+ 
+
+
   return 0;
 }
